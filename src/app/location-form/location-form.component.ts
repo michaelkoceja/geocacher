10980b9927c48d697309b3a8ad175697c5970c76
michaelkoceja <michael.koceja@gmail.com>
2017-07-27 09:49:23 -0700

    api calls to the console when lat and long fields are submited, displaying the locations formatted_address

--- src/app/location-form/location-form.component.ts
+++ src/app/location-form/location-form.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { GeocallService } from '../geocall.service'
 
 @Component({
   selector: 'app-location-form',
   templateUrl: './location-form.component.html',
-  styleUrls: ['./location-form.component.css']
+  styleUrls: ['./location-form.component.css'],
+  providers: [GeocallService]
 })
 export class LocationFormComponent implements OnInit {
+  location: any[]=null;
 
-  constructor() { }
+  constructor(private geoCallService: GeocallService) { }
+
+  getLocation(lat: number, long: number) {
+  this.geoCallService.getByLatAndLong(lat, long).subscribe(response => {
+      this.location = response.json();
+      console.log(response.json().results[0].formatted_address);
+  });
+}
 
   ngOnInit() {
   }
